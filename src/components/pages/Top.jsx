0ae31caf24da5e3e1e242d865c559a31f7837f49
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-30 14:14:42 +0900

    Add Select Users button on top page

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+
 export const Top = () => {
+  const onClickAdmin = () => {
+    alert("Admin");
+  };
+  const onClickGeneral = () => {
+    alert("General");
+  };
   return (
     <SContainer>
       <h2>Top page!</h2>
+      <SecondaryButton onClick={onClickAdmin}>Admin User</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>Nomal User</SecondaryButton>
     </SContainer>
   );
 };
